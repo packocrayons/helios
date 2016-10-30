fa7d4a4297af635e4694a16dd239dc750248fd86
packocrayons <gibson.brydon@gmail.com>
2016-10-30 13:41:52 -0400

    Added some methods, and wrote the taskscheduler. It is still too juvenile to test at the moment, however a lot of the code is trivial.
    A file read loop is still required and determining how we are going to get time is also required.
    
    It looks like we are going to have to fork() processes for IOMachine(), and give those forked processes mutually exclusive access to waitingList, so that they can
    put themselves into the waiting list when their IO is done (found in PCB*->IODuration)

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # helios
+
+Jason Van Kerkhoven (100974276)
+Brydon Gibson (100975274)
+
 helios is an operating-system-to-be that even if it fails, will still be better than Solaris
 
-Test line
+Current implementation is a first-come-first-serve task scheduler. It uses linked lists to store PCB's in a wait set and a ready set, plus a PCB running in the processor (there should be one in the processor at almost all times). All PCB's should be found there, at least for the time being.
+
+The current 'main' binary was built on a linux system. It is included because I'm too lazy to learn how gitignore works.
--- main.c
+++ main.c
@@ -16,14 +16,41 @@ typedef struct PCB{
 	PCB* next; //for linked list - could make another wrapper struct with just a PCB and a head, may do that later
 } PCB;
 
-void taskScheduler(){
-	//switch processes
+// enum TaskSchedulerJob{  This might be used later
+// 	KICK,
+
+// }
+
+
+PCB* getLastElement(PCB* list){
+	for (/*list*/; list->next != NULL; list = list->next); //walk the list until next is null and then return
+	return list;
 }
 
 
+/*These are global variables that contain the readyList and waitingList.
+These are global so that the taskScheduler can access them easily. They could be passed as references to the taskScheduler, however that involves stacking pointers
+which wastes CPU time since nothing is using the CPU while we wait for those parameters to be stacked.
+*/
+PCB* readyListHead, waitingListHead; //newListHead, terminatedListHead; //Aren't needed right now - new goes straight to ready and terminated dies immediately
+PCB* currentProcess; //this is equivalent to "the processor"
+
+void taskScheduler(/*TaskSchedulerJob t*/){
+	if(currentProcess != NULL){ //If there was a process in the processor - this means we're kicking it out, so we have to save it's PCB at the end of the list
+		getLastElement(readyListHead)->next = currentProcess; //put the current process at the end of the readyList
+	}//otherwise the process has already put itself at the end of the wait list, we can use the processer as we want
+	currentProcess = readyListHead; //the first thing we always do is dispatch a process to the processor
+	PCB* toFree = readyListHead;
+	readyListHead = readyListHead->next; //the process we put in the processor is no longer in the readyList
+	free(toFree); //free the one at the beginning, it's in currentProcess now
+}
+
+void IOMachine(int IOTime, PCB* putThisInWaitList){
+	//somehow have to wait and then put putThisInWaitList in the waitingList after a set period of time. Ideally a fork with shared memory, however that makes things much much much more complicated	
+}
+
 int main(int argc, char const *argv[])
 {
-	PCB* readyListHead, waitingListHead; //newListHead, terminatedListHead; //Aren't needed right now - new goes straight to ready and terminated dies immediately
 	/*Start of a line-by-line file read loop*/
 	/*Read each processes process control block (PCB) and put it into an array (or linked list) of PCB's.*/
 	char str[5]; //= However we get a list of parameters (looks like probably files read line by line)
@@ -46,15 +73,19 @@ int main(int argc, char const *argv[])
 	
 	/*End of a line-by-line file read loop*/
 
+
 	int lastTime; //=get current time
-	while(1){
-		int curTime;//= get_current_time (can't quite figure out how to do this)
+	while(1){ //always
+		int curTime;//= get_current_time (can't quite figure out how to do this) 
 		if (curTime - lastTime > 1000) { //1000uSec = 1 millisecond per process maximum
 			lastTime = curTime;
-			//put the current process in the ready list then call the taskScheduler
-			taskScheduler();
+			taskScheduler(); //kick it by calling the taskscheduler - it handles kicking the process
 		}
-		//if (The process decides to do IO){}
+		if ((currentProcess->requiredCPU /*- current time*/) % currentProcess->IOFrequency == 0){ //IF it's time to do IO - this will run indefinitely until - currentTime is added
+			IOMachine(currentProcess->IODuration, currentProcess); //the IOMachine (if implementation as desired is possible) will put the process in the wait list after the IO is completed
+			currentProcess=NULL; //there's another existing pointer to this (in the stack frame for IOMachine - to be in waitingList)
+			taskScheduler(); //put something new in the processor
+		}
 	}
-	return 0;
+	|return 0;
 }
